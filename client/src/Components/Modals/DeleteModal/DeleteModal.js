725a8d15ac2e3565f3dcd7ffc04308d465d592d1
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2023-10-30 17:15:01 +0530

    delete comit

--- client/src/Components/Modals/DeleteModal/DeleteModal.js
+++ client/src/Components/Modals/DeleteModal/DeleteModal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Modal from 'react-modal';
 import "./DeleteModal.css"; 
-// Modal.setAppElement('#root'); 
+Modal.setAppElement('#root'); 
 
 const DeleteModal = ({ isOpen, onRequestClose, onDelete }) => {
   return (
